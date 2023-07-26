a05d353759db06a29e87ffe5e6f050268adfa7cc
boujrada yassine <yassine.boujrada@gmail.com>
2023-07-26 21:50:44 +0100

    header componenent

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import {Home} from "@/components/Home";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+    <div className="App">
+      <Header />
+    </div>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route element={<PrivateRoutes />}>
+    //         <Route path="/" element={<Root />}>
+    //             <Route path="home" element={<Home />} />
+    //             <Route path="aboutus" element={<About />} />
+    //         </Route>
+    //     </Route>
+    //     <Route path="*" element={<h1>mmmmm</h1>} />
+    //   </Routes>
+    // </BrowserRouter>
   );
 }
 
