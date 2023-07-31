7b816c4f94e4963ec1f51cb1e807dcf5f8ce0ca7
boujrada yassine <yassine.boujrada@gmail.com>
2023-07-31 01:46:28 +0100

    companies section

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
+import Company from "./components/Companies/Company";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/home";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <div>
+        <Header />
+        <Home />
+      </div>
+      <Company />
     </div>
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route element={<PrivateRoutes />}>
-    //         <Route path="/" element={<Root />}>
-    //             <Route path="home" element={<Home />} />
-    //             <Route path="aboutus" element={<About />} />
-    //         </Route>
-    //     </Route>
-    //     <Route path="*" element={<h1>mmmmm</h1>} />
-    //   </Routes>
-    // </BrowserRouter>
   );
 }
 
---
+++ src/components/Companies/Company.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './company.css'
+
+const Company = () => {
+  return (
+    <section className="home-wrapper">
+      <div className="paddings innerWidth c-container flexCenter">
+        <img src="./prologis.png" alt="prologis" />
+        <img src="./tower.png" alt="prologis" />
+        <img src="./equinix.png" alt="prologis" />
+        <img src="./realty.png" alt="prologis" />
+      </div>
+    </section>
+  )
+}
+
+export default Company
